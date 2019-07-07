ced381ab8e718ab2b046aa2e8383c5877814d991
suresh <suresh.avlm@gmail.com>
2019-07-07 09:59:32 +0530

    thirdparty modules demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'thirdparty-demo';
+
+  constructor() {
+  	$(document).ready(function() {
+  		$("button").click(function() {
+  			$("h2").hide();
+  		});
+  	});
+  }
 }
